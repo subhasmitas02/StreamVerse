d43291d44e7e0b063253b65f8463a1b11efc13d4
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2026-02-08 21:49:54 +0530

    Setup Service Discovery (Eureka) and Config Server

---
+++ services/config-server/src/test/java/com/subhasmita/streamverse/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhasmita.streamverse.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
